f125ca2bba495f7fa5121a6d84d01cedf812b293
Stilen Adams <stilenadams@mymail.mines.edu>
2019-11-21 13:03:03 -0700

    4th update

--- Week12-utility.py
+++ Week12-utility.py
@@ -5,8 +5,33 @@ def PrintOutput(string):
 def LoadFile(file):
     f=open(file,'r')
     lines=f.read()
-    print("OUTPUT>",lines)
+    Lines=lines.split(" ")
+    print("OUTPUT>",Lines)
+    return Lines
     
 def UpdateString(string1,string2,index):
     return string2[:index]+string1+string2[index+1:]
 
+def FindWordCount(List,string):
+    count=List.count(string)
+    print("OUTPUT>",count)
+def ScoreFinder(players,scores,name):
+    if name in players:
+        
+        position=int(players.index(name))
+        score=scores[position]
+        print("OUTPUT>",name,"got a score of ",score)
+    else:
+        print("OUTPUT> Player not found")
+    
+def Union(players,scores):
+    for i in players:
+        scores.append(i)
+    print("OUTPUT>" ,scores)
+
+def Intersection(List1,List2):
+    List3=[]
+    for i in List1:
+        if i in List2:
+            List3.append(i)
+    print("OUTPUT>",List3)
